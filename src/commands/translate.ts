c84506773f5c2cc0873089e1a6595706cd8a2521
Tzahi nemet <tzahi.nemet@gmail.com>
2022-06-11 11:26:22 +0300

    updated usage with the services on cli handler

--- src/commands/translate.ts
+++ src/commands/translate.ts
@@ -1,11 +1,11 @@
 import { Arguments, CommandBuilder } from "yargs";
+import { TranslateService } from "../services/translate-service";
+import { Logger } from "../services/logging";
 
 type Options = {
   text: string;
   debug: boolean | undefined;
 };
-
-type MapKvpScore = { [key: string]: string | number };
 
 const wordsOrder: string[] = [
   "A",
@@ -46,95 +46,31 @@ export const builder: CommandBuilder<Options, Options> = (yargs) =>
     })
     .positional("text", { type: "string", demandOption: true });
 
-let isDebugEnabled = false;
-const writeLog = (message: string, isDebug?: boolean | undefined) => {
-  let shouldWrite = true;
-  if (isDebug && !isDebugEnabled) {
-    shouldWrite = false;
-  }
-  if (shouldWrite) console.log(message);
-};
-
 export const handler = (argv: Arguments<Options>): void => {
   const { text, debug } = argv;
+
+  let isDebugEnabled = false;
   if (debug) isDebugEnabled = true;
 
-  writeLog(`received input : ${text}`);
+  const logger = new Logger(isDebugEnabled);
+  logger.WriteLog(`received input : ${text}`);
 
-  writeLog(`building map scoring collection`, true);
-  for (let index = 0; index < wordsOrder.length; index++) {
-    vanillaMap[wordsOrder[index]] = index;
-  const vanillaMap: MapKvpScore = {};
-    writeLog(`score for ${wordsOrder[index]} is ${index}`, true);
-  }
+  const service = new TranslateService(wordsOrder, logger);
+  const wordsOrdered = service.translate(text);
 
-  const csv = text.split(" ");
-  const csvSanitized = csv.map((word) => word.replace(/[^a-zA-Z]/g, ""));
-  writeLog(`splitting the text to words produced ${csv.length} words`, true);
+  logger.WriteLog(
+    `*******************************************************`,
+    false
+  );
 
-  writeLog(`sorting the words...`, true);
-  const sorted = csvSanitized.sort((word1, word2) => {
-    const w1Upper = word1.toUpperCase();
-    const w2Upper = word2.toUpperCase();
-
-    writeLog(`inspecting word1 ${w1Upper} vs ${w2Upper}`, true);
-
-    for (let i = 0; i < w1Upper.length; i++) {
-      if (w2Upper.length < i) {
-        writeLog(
-          `inspecting word1 ${w1Upper} vs ${w2Upper} in position ${i} the second word ends - the longer word should be after the shorter`,
-          true
-        );
-        return -1; //word 1 is equal until now and it's longer , it should go after word 2
-      }
-      const letter1 = w1Upper[i];
-      const letter2 = w2Upper[i];
-
-      if (letter1 == letter2) {
-        writeLog(
-          `inspecting word1 ${w1Upper} vs ${w2Upper} in position ${i} they are the same - jumping to the next letters`,
-          true
-        );
-        continue; //same letters - continue down the string...
-      }
-
-      if (vanillaMap[letter1] > vanillaMap[letter2]) {
-        writeLog(
-          `inspecting word1 ${w1Upper} vs ${w2Upper} in position ${i} the letter1 ${letter1} (position ${vanillaMap[letter1]}) is smaller than letter2 ${letter2} (position ${vanillaMap[letter2]})`,
-          true
-        );
-        return 1;
-      } else if (vanillaMap[letter1] < vanillaMap[letter2]) {
-        writeLog(
-          `inspecting word1 ${w1Upper} vs ${w2Upper} in position ${i} the letter1 ${letter1} (position ${vanillaMap[letter1]}) is larger than letter2 ${letter2} (position ${vanillaMap[letter2]})`,
-          true
-        );
-        return -1;
-      }
-    }
-
-    if (w1Upper.length < w2Upper.length) {
-      writeLog(
-        `inspecting word1 ${w1Upper} vs ${w2Upper} word 2 is equal until now and it's longer , it should go after word 1`,
-        true
-      );
-      return -1;
-    }
-
-    writeLog(
-      `inspecting word1 ${w1Upper} vs ${w2Upper} words are the same`,
-      true
-    );
-    return 0; //words are equal
-  });
-
-  writeLog(`*******************************************************`, false);
-
-  for (const word of sorted) {
+  for (const word of wordsOrdered) {
     process.stdout.write(`${word} `);
   }
-  writeLog(``);
-  writeLog(`*******************************************************`, false);
+  logger.WriteLog(``);
+  logger.WriteLog(
+    `*******************************************************`,
+    false
+  );
 
   process.exit(0);
 };
